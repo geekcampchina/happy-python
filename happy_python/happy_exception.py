279b485d0ccd7a804a83a8de3fd03a99b8df79b6
FifiLyu <fifilyu@gmail.com>
2025-01-24 15:32:43 +0800

    add new HappyPyException

--- happy_python/happy_exception.py
+++ happy_python/happy_exception.py
@@ -1,3 +1,19 @@
+import traceback
+
+from happy_python import HappyLog
+
+
 class HappyPyException(Exception):
-    def __init__(self, err='Happy Python Error'):
+    hlog = HappyLog.get_instance()
+
+    @staticmethod
+    def get_stack(e: Exception):
+        return '\n'.join(traceback.format_exception(e))
+
+    def __init__(self, err='Happy Python Error', e: Exception = None):
+        if e:
+            self.hlog.error(HappyPyException.get_stack(e))
+        else:
+            self.hlog.error(err)
+
         Exception.__init__(self, err)
