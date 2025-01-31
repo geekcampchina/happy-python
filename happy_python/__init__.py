630dca2b1d5811fdb3f46026f943a2b35e8016eb
FifiLyu <fifilyu@gmail.com>
2025-01-31 15:25:34 +0800

    修复导入错误

--- happy_python/__init__.py
+++ happy_python/__init__.py
@@ -1,6 +1,6 @@
-from happy_python.happy_exception import HappyPyException
 # 基础代码必须最先导入
 from happy_python.happy_log import HappyLog
+from happy_python.happy_exception import HappyPyException
 from happy_python.mail import EmailAddr, HappyEmail
 from happy_python.parameter_manager import ParameterManager
 from happy_python.happy_config import HappyConfigBase
